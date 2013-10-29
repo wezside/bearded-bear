8465a29853758934d83fb9bbbbd3dc74cce8ddba
wezside <wesley@wezside.co.za>
2013-10-29 12:00:47 +0200

    Initial commit

---
+++ src/testApp.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "ofMain.h"
+#include "Leap.h"
+
+using namespace Leap;
+
+class SampleListener : public Listener 
+{
+	public:
+		virtual void onInit(const Controller&);
+		virtual void onConnect(const Controller&);
+		virtual void onDisconnect(const Controller&);
+		virtual void onExit(const Controller&);
+		virtual void onFrame(const Controller&);
+		virtual void onFocusGained(const Controller&);
+		virtual void onFocusLost(const Controller&);
+};
+
+class testApp : public ofBaseApp
+{
+	private:
+		Controller controller;
+		SampleListener listener;
+		ofCamera camera;
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+};
